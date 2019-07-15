54ef60ff949ab7d9a84d5ffd33f94405f5a85711
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-07-15 16:50:16 +0300

    Update AllUsersServlet.java

--- src/main/java/controller/AllUsersServlet.java
+++ src/main/java/controller/AllUsersServlet.java
@@ -9,9 +9,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
+
 
 @WebServlet(value = "/admin/users")
 public class AllUsersServlet extends HttpServlet {
@@ -21,15 +21,9 @@ public class AllUsersServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request,
                          HttpServletResponse response) throws ServletException, IOException {
-        final HttpSession session = request.getSession();
-        final String role = (String) session.getAttribute("role");
-        if (role.equals("admin")) {
-            List<User> allUsers = userService.getAllUsers();
-            request.setAttribute("users", allUsers);
-            request.getRequestDispatcher("/users.jsp").forward(request, response);
-        } else {
-            response.sendRedirect("/");
-        }
+        List<User> allUsers = userService.getAllUsers();
+        request.setAttribute("users", allUsers);
+        request.getRequestDispatcher("/users.jsp").forward(request, response);
     }
-
 }
+
