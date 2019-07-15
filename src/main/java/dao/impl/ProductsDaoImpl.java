a418b3f11d6a6a1b912518a9592343a158288c56
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-07-15 16:54:59 +0300

    Update ProductsDaoImpl.java

--- src/main/java/dao/impl/ProductsDaoImpl.java
+++ src/main/java/dao/impl/ProductsDaoImpl.java
@@ -12,16 +12,15 @@ import java.util.stream.Stream;
 public class ProductsDaoImpl implements ProductsDao {
 
     private static final Logger logger = Logger.getLogger(ProductsDaoImpl.class);
-    private Product product;
 
     @Override
     public void addProduct(Product product) {
         Database.products.add(product);
     }
-
+    
     @Override
     public void addProduct(Long id, String name, String description, Double price) {
-        product = new Product(id, name, description, price);
+        Product product = new Product(id, name, description, price);
         Database.products.add(product);
         logger.info("Product " + product + "added in DB");
     }
