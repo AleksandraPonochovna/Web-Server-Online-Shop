d2836c9d4ad6d022c1cfd52c735ce3d0505ab154
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-07-24 13:02:23 +0300

    Update OrderDaoImpl.java

--- src/main/java/dao/impl/OrderDaoImpl.java
+++ src/main/java/dao/impl/OrderDaoImpl.java
@@ -1,22 +1,23 @@
 package dao.impl;
 
-import com.sun.org.apache.xpath.internal.operations.Or;
 import dao.OrderDao;
-import model.*;
+import model.Code;
+import model.Order;
+import model.User;
 import org.apache.log4j.Logger;
 import util.DBConnector;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Optional;
-
 
 public class OrderDaoImpl implements OrderDao {
 
     private static final Logger logger = Logger.getLogger(OrderDaoImpl.class);
     private static final String ADD_ORDER_IN_DB = "INSERT INTO `order` (first_name, last_name, number_phone, street_name, " +
             "house_number, basket_id) VALUES ('%s', '%s', '%s', '%s', '%s', %d)";
-    private static final String GET_BASKET_ID_FROM_DB = "SELECT basket_id FROM `order` INNER JOIN basket" +
-            " WHERE order.id = %d LIMIT 1";
     private static final String GET_ORDER_BY_USER = "SELECT order.id, first_name, last_name, number_phone, street_name," +
             " house_number, code FROM `order` INNER JOIN basket INNER JOIN code WHERE basket_id = basket.id " +
             "AND user_id = %d AND user_id = code.id_user LIMIT 1";
@@ -32,21 +33,6 @@ public class OrderDaoImpl implements OrderDao {
         } catch (SQLException e) {
             logger.info("Order " + order + " can't be added in DB.");
         }
-    }
-
-    @Override
-    public Long getBasketIdForOrder(Order order) {
-        try (Connection connection = DBConnector.connect()) {
-            String getBasketId = String.format(GET_BASKET_ID_FROM_DB, order.getId());
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(getBasketId);
-            if (resultSet.next()) {
-                return resultSet.getLong("id");
-            }
-        } catch (SQLException e) {
-            logger.info("Basket ID for " + order + " can't be taken from DB.");
-        }
-        return null;
     }
 
     @Override
