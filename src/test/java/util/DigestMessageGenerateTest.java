a92d0b96d503f09627e9a374ed0abacd09a65770
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-08-07 11:05:15 +0300

    Update DigestMessageGenerateTest.java

--- src/test/java/util/DigestMessageGenerateTest.java
+++ src/test/java/util/DigestMessageGenerateTest.java
@@ -1,3 +1,4 @@
+
 package util;
 
 import org.junit.Assert;
@@ -5,13 +6,25 @@ import org.junit.Test;
 
 public class DigestMessageGenerateTest extends DigestMessageGenerate {
 
-    @Test
-    public void encryptSha256AndSalt1() {
-        String password = "admin";
-        String salt = "123456";
-        String actualPassword = DigestMessageGenerate.encryptSha256AndSalt(password, salt);
-        String expectedResult = DigestMessageGenerate.encryptSha256(salt + password);
-        Assert.assertEquals(expectedResult, actualPassword);
-    }
+   @Test
+   public void encryptSha256AndSalt1() {
+      String password = "admin";
+      String salt = "123456";
+      String actualPassword = DigestMessageGenerate.encryptSha256AndSalt(password, salt);
+      String expectedResult = DigestMessageGenerate.encryptSha256(salt + password);
+      Assert.assertEquals(expectedResult, actualPassword);
+   }
+
+   private static String encryptSha256(String password) {
+      MessageDigest digest = null;
+      try {
+         digest = MessageDigest.getInstance("SHA-256");
+         byte[] encodedHash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+         return bytesToHex(encodedHash);
+      } catch (NoSuchAlgorithmException e) {
+         logger.error("Message Digest don't have an instance.", e);
+      }
+      return null;
+   }
 
 }
